591404f3658b82cb6f1a694d60d82b6570b39121
Leo <leoliu5550@gmail.com>
2022-12-18 14:16:00 +0800

    model1

--- model1.py
+++ model1.py
@@ -28,7 +28,7 @@ architecture_config = [
 class CNNBlock(nn.Module):
     def __init__(self,in_channels,out_channels,**kwargs):
         super(CNNBlock,self).__init__()
-        self.conv = nn.Conv2d(in_channels,out_channels,bias=False,**kwargs)
+        self.conv = nn.Conv2d(in_channels,out_channels,bias=False,**kwargs) #
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
 
@@ -106,7 +106,7 @@ class Yolov1(nn.Module):
             nn.Linear(1024 * S * S,1024),
             nn.Dropout(0.5),
             nn.LeakyReLU(0.1),
-            nn.Linear(1024,S * S *(C+B*5)),
+            nn.Linear(1024,S * S * (C+B*5)),
         )
         return model
 
